da49c328fa335ec299e5fca1da1c15dbeaf5bc84
Xing Jin <xingvictoriajin@gmail.com>
2016-08-11 00:33:56 -0400

    Create MaximumPathSumBinaryTreeI.java

---
+++ MaximumPathSumBinaryTreeI.java
@@ -0,0 +1,38 @@
+/*
+Maximum Path Sum Binary Tree I
+Given a binary tree in which each node contains an integer number. 
+Find the maximum possible sum from one leaf node to another leaf node.
+If there is no such path available, return Integer.MIN_VALUE(Java)/INT_MIN (C++).
+
+Examples
+
+  -15
+
+  /    \
+
+2      11
+
+     /    \
+
+    6     14
+
+The maximum path sum is 6 + 11 + 14 = 31.
+
+How is the binary tree represented?
+
+We use the level order traversal sequence with a special symbol "#" denoting the null node.
+
+For Example:
+
+The sequence [1, 2, 3, #, #, 4] represents the following binary tree:
+
+    1
+
+  /   \
+
+ 2     3
+
+      /
+
+    4
+*/
