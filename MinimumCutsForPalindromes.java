bfd1ac1d2f5e47ed19559d53b5b204895580a576
Xing Jin <xingvictoriajin@gmail.com>
2016-08-11 00:32:46 -0400

    Minimum Cuts For Palindromes

---
+++ MinimumCutsForPalindromes.java
@@ -0,0 +1,18 @@
+/*
+Given a string, a partitioning of the string is a palindrome partitioning if 
+every substring of the partition is a palindrome. Determine the fewest cuts 
+needed for a palindrome partitioning of a given string.
+Assumptions
+The given string is not null
+Examples
+
+“a | babbbab | bab | aba” is a palindrome partitioning of “ababbbabbababa”.
+
+The minimum number of cuts needed is 3.
+*/
+public class Solution {
+  public int minCuts(String input) {
+    // Write your solution here.
+    return 0;
+  }
+}
