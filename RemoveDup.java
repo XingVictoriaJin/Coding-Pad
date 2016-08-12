3d25dc2e72b217e19c6911c08e46ae509b5743fa
Xing Jin <xingvictoriajin@gmail.com>
2016-08-12 13:10:26 -0400

    Update RemoveDup.java

--- RemoveDup.java
+++ RemoveDup.java
@@ -12,5 +12,15 @@
 		  return Arrays.copyOfRange(input, 0, slow+1);
 		}
 		
-		
-		
+/*if <=2 can be kept for each item */
+	public int[] removeDup( int[] input){
+		  // assume input is not null
+		  int slow = 2; // left part which is not including slow is processed part 
+		  for( int fast = 2; fast < input.length; fast++){
+		      if( input[fast] == input[slow - 2]){
+		        continue;
+		      }
+		      input[slow++]=input[fast];
+		  }
+		  return Arrays.copyOfRange(input, 0, slow);
+		}
