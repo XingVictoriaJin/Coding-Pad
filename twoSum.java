2bcb8c3bf6c6572c2a430a8161f73d66d17b24c9
Xing Jin <xingvictoriajin@gmail.com>
2016-08-19 10:26:48 -0400

    Create twoSum.java

---
+++ twoSum.java
@@ -0,0 +1,37 @@
+ /* find x element from a (sorted/unsorted) array such that their sum is equal to a target value
+  * for unsorted one: sorted first O(n)
+  * for sorted one: 
+  * opitonA-->step1: hashset  step2: target-x search so it is O(n) for time/space
+  * optionB-->two pointer: i and j... i++ ... j-- A[i] + A[j] compare to sum
+  * additional: what about duplicate values there?
+  * */
+	public List<List<Integer>> twoSum(int[] array, int target){
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
+		int n = array.length;
+		int i = 0;
+		int j = n - 1;
+		while( i <= n-1 && j > i){
+			//ignore all the consecutive duplicate values when we want 
+			// to determine the smaller element of the pair
+			if(i > 0 && array[i] == array[i-1]){
+				i++;
+				continue;
+			}
+			if(array[i] + array[j] == target){
+				List<Integer> pair = new ArrayList<Integer>();
+				pair.add(array[i]);
+				pair.add(array[j]);
+				result.add(pair);
+				i++;
+				j--;
+			}else if(array[i] + array[j] > target){
+				j--;
+			}
+			else{
+				i++;
+			}
+		}
+		
+		return result;
+		
+	}
