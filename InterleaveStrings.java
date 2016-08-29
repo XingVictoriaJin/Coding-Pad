9287b30a60a960fa738613ccd18699abae6f5d1c
Xing Jin <xingvictoriajin@gmail.com>
2016-08-29 15:01:43 -0400

    Update InterleaveStrings.java

--- InterleaveStrings.java
+++ InterleaveStrings.java
@@ -1,1 +1,17 @@
+/*
+Given three strings A, B and C. Determine if C can be created by merging A and B in a way that maintains the relative order of the characters in A and B.
 
+Assumptions
+
+none of A, B, C is null
+Examples
+
+C = "abcde", A = "acd", B = "be", return true
+C = "abcde", A = "adc", B = "be", return false
+*/
+public class Solution {
+  public boolean canMerge(String a, String b, String c) {
+    // Write your solution here.
+    return false;
+  }
+}
