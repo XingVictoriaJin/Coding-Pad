03ef36c3bd930752592b89ea16350573d944df11
Xing Jin <xingvictoriajin@gmail.com>
2016-08-29 15:16:25 -0400

    Update InterleaveStrings.java

--- InterleaveStrings.java
+++ InterleaveStrings.java
@@ -10,8 +10,28 @@ C = "abcde", A = "acd", B = "be", return true
 C = "abcde", A = "adc", B = "be", return false
 */
 public class Solution {
-  public boolean canMerge(String a, String b, String c) {
-    // Write your solution here.
-    return false;
-  }
+	  public boolean canMerge(String a, String b, String c) {
+		    // assume a and b dont have common characters. 
+		    // assume a , b and c are not null, and not empty
+		   char[] strA = a.toCharArray();
+		   char[] strB = b.toCharArray();
+		   char[] strC = c.toCharArray();
+		   int i = 0;
+		   int j = 0;
+		   int k = 0;
+		   while(k < c.length()){
+			   if(i < a.length() && strC[k] == strA[i]){
+				   k++;
+				   i++;
+			   }else if(j < b.length() && strC[k] == strB[j]){
+				   k++;
+				   j++;
+			   }else{
+				   return false;
+			   }
+		   }
+		  
+		    return true;
+		  }
 }
+
