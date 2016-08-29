6c74f9d27f04d47e65f9695fadf7dbc7e2b6dfc2
Xing Jin <xingvictoriajin@gmail.com>
2016-08-29 10:18:39 -0400

    Update InterleaveStrings.java

--- InterleaveStrings.java
+++ InterleaveStrings.java
@@ -1,18 +1,1 @@
-public class MajorityNumberI {
-  public int majority(int[] array){
-	  int cur = array[0];
-	  int count = 1;
-	  for(int i = 1; i < array.length; i++){
-		  if(count == 0){
-			cur = array[i];  
-			count++;// I missed this part!
-		  }
-		  else if( cur != array[i]){
-			  count--;
-		  }else{
-			  count++;
-		  }
-	  }
-   return cur;
-  }
-}
+
