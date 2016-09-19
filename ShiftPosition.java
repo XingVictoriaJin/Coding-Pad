ac12f8e2cb314228db13712b0e39e2271f0ab6e3
Xing Jin <xingvictoriajin@gmail.com>
2016-09-19 00:16:35 -0400

    Create ShiftPosition.java

---
+++ ShiftPosition.java
@@ -0,0 +1,22 @@
+/*
+Data Structure
+Shift Position
+Given an integer array A, A is sorted in ascending order first then shifted by an arbitrary number of positions, For Example, A = {3, 4, 5, 1, 2} (shifted left by 2 positions). Find the index of the smallest number.
+
+Assumptions
+
+There are no duplicate elements in the array
+Examples
+
+A = {3, 4, 5, 1, 2}, return 3
+A = {1, 2, 3, 4, 5}, return 0
+Corner Cases
+
+What if A is null or A is of zero length? We should return -1 in this case.
+*/
+
+public class Solution {
+  public int shiftPosition(int[] array) {
+    // Write your solution here
+    return 0;
+  
