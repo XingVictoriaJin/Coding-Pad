68dd03ac798a43b735edd51a7bcf19fadaa821e0
Xing Jin <xingvictoriajin@gmail.com>
2016-09-19 00:07:54 -0400

    Create search.java

---
+++ search.java
@@ -0,0 +1,26 @@
+/*
+Data Structure
+Search In Shifted Sorted Array I
+Given a target integer T and an integer array A, A is sorted in ascending order first, then shifted by an arbitrary number of positions.
+
+For Example, A = {3, 4, 5, 1, 2} (shifted left by 2 positions). Find the index i such that A[i] == T or return -1 if there is no such index.
+
+Assumptions
+
+There are no duplicate elements in the array.
+Examples
+
+A = {3, 4, 5, 1, 2}, T = 4, return 1
+A = {1, 2, 3, 4, 5}, T = 4, return 3
+A = {3, 5, 6, 1, 2}, T = 4, return -1
+Corner Cases
+
+What if A is null or A is of zero length? We should return -1 in this case.
+*/
+
+public class Solution {
+  public int search(int[] array, int target) {
+    // Write your solution here
+    return 0;
+  }
+}
