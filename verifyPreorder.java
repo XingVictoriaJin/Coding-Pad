a156b44c36151758c7586d96a0b6dd53b4a24d91
Xing Jin <xingjin3131@gmail.com>
2016-10-10 14:59:50 -0400

    Update verifyPreorder.java

--- verifyPreorder.java
+++ verifyPreorder.java
@@ -6,6 +6,22 @@ Could you do it using only constant space complexity
 */
 public class Solution {
     public boolean verifyPreorder(int[] preorder) {
-        
+		  if( preorder == null) {return false;}
+          Deque<Integer> stack = new LinkedList<Integer>();
+		  stack.offerFirst(preorder[0]);
+		  int i = 1;
+				while (i < preorder.length 
+				  &&  (stack.isEmpty() || preorder[i] < stack.peekFirst() ) ) {
+					stack.offerFirst(preorder[i]);
+					i++;
+				}
+				while( i < preorder.length 
+					&& !stack.isEmpty() && stack.peekFirst() < preorder[i]){
+					stack.pollFirst();
+				}
+			if(stack.size() == 1){
+				return true;
+			}
+		return false;
     }
 }
